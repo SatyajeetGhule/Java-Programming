c219c57fa6b31ab83e774203c3ac12cc33b7fb00
SatyajeetGhule <134911172+SatyajeetGhule@users.noreply.github.com>
2025-11-06 14:03:23 +0530

    Add files via upload

---
+++ Classwork/Program21.java
@@ -0,0 +1,21 @@
+//Best Approch
+
+class Marvellous
+{
+    public void Display()   
+    {
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+        System.out.println("Jay Ganesh...");
+    }
+}
+class Program21
+{
+    public static void main(String arg[])
+    {
+        Marvellous mobj = new Marvellous();
+        mobj.Display(); 
+        
+    }
+}
